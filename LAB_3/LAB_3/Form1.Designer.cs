98c826002ca63bac11bd565fc8cfa5dbd6795f75
thirteenmd <andreacojocaru@gmail.com>
2017-03-11 23:53:05 +0200

    implmented Basic Level task 1

--- LAB_3/LAB_3/Form1.Designer.cs
+++ LAB_3/LAB_3/Form1.Designer.cs
@@ -29,22 +29,34 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(121, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(522, 350);
+            this.pictureBox1.Size = new System.Drawing.Size(599, 223);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Draw 5 Lines";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(534, 362);
+            this.ClientSize = new System.Drawing.Size(732, 510);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +69,9 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+
+
     }
 }
 
--- LAB_3/LAB_3/Form1.cs
+++ LAB_3/LAB_3/Form1.cs
@@ -12,14 +12,34 @@ namespace LAB_3
 {
     public partial class Form1 : Form
     {
+        Graphics drawArea;
         public Form1()
         {
             InitializeComponent();
+            drawArea = pictureBox1.CreateGraphics();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pen blackPen = new Pen(Color.Black);
+            drawArea.DrawLine(blackPen, 10, 20, 150, 20);
+
+            Pen greenPen = new Pen(Color.Green, 5);
+            drawArea.DrawLine(greenPen, 10, 40, 150, 40);
+
+            Pen yellowPen = new Pen(Color.Yellow, 10);
+            drawArea.DrawLine(yellowPen, 10, 60, 150, 60);
+
+            Pen grayPen = new Pen(Color.Gray, 15);
+            drawArea.DrawLine(grayPen, 10, 85, 150, 85);
+
+            Pen redPen = new Pen(Color.Red, 20);
+            drawArea.DrawLine(redPen, 10, 120, 150, 120);
         }
     }
 }
