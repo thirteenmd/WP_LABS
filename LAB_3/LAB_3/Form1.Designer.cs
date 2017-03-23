698a33bd4cdab15dd492c6264d771396bd2c2dd4
thirteenmd <andreacojocaru@gmail.com>
2017-03-23 23:08:44 +0200

    completed Basic Level

--- LAB_3/LAB_3/Form1.Designer.cs
+++ LAB_3/LAB_3/Form1.Designer.cs
@@ -30,38 +30,125 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(121, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(200, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(599, 223);
+            this.pictureBox1.Size = new System.Drawing.Size(613, 486);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(6, 19);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.Size = new System.Drawing.Size(162, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Draw 5 Lines";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Draw 2 Bezier Curves";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(162, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Draw 4 Plane Objects";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(11, 19);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(157, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Draw a custom bitmap image";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(182, 111);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Basic Level";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Location = new System.Drawing.Point(12, 129);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(182, 109);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Normal Level";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(11, 46);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(157, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Fill 2 object with gradient";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(11, 75);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(157, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Draw a Bezier curve using mouse";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(732, 510);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(825, 510);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -70,6 +157,13 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
 
 
     }
--- LAB_3/LAB_3/Form1.cs
+++ LAB_3/LAB_3/Form1.cs
@@ -21,11 +21,13 @@ namespace LAB_3
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            drawArea.Clear(Color.White);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            drawArea.Clear(Color.White);
+            
             Pen blackPen = new Pen(Color.Black);
             drawArea.DrawLine(blackPen, 10, 20, 150, 20);
 
@@ -40,6 +42,112 @@ namespace LAB_3
 
             Pen redPen = new Pen(Color.Red, 20);
             drawArea.DrawLine(redPen, 10, 120, 150, 120);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            drawArea.Clear(Color.White);
+            Pen blackPen = new Pen(Color.Black, 3);
+
+            Point start = new Point(10, 10);
+            Point control1 = new Point(20, 10);
+            Point control2 = new Point(90, 50);
+            Point end1 = new Point(50, 100);
+            Point control3 = new Point(60, 150);
+            Point control4 = new Point(60, 250);
+            Point end2 = new Point(50, 300);
+            Point[] bezierPoints =
+             {
+                 start, control1, control2, end1,
+                 control3, control4, end2
+             };
+            drawArea.DrawBeziers(blackPen, bezierPoints);
+
+            Pen redPen = new Pen(Color.Red, 3);
+
+            start = new Point(50, 50);
+            control1 = new Point(20, 10);
+            control2 = new Point(120, 75);
+            end1 = new Point(150, 200);
+            control3 = new Point(160, 150);
+            control4 = new Point(160, 250);
+            end2 = new Point(150, 300);
+            Point[] bezierPoints2 =
+             {
+                 start, control1, control2, end1,
+                 control3, control4, end2
+             };
+            drawArea.DrawBeziers(redPen, bezierPoints2);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            drawArea.Clear(Color.White);
+
+            Pen redPen = new Pen(Color.Red, 3);
+            drawArea.DrawEllipse(redPen, new Rectangle(10, 10, 100, 100));
+
+            Pen yellowPen = new Pen(Color.Yellow, 5);
+            drawArea.DrawRectangle(yellowPen, new Rectangle(150, 0, 150, 150));
+
+            Pen grayPen = new Pen(Color.Gray, 1);
+            Point p1 = new Point(0, 150);
+            Point p2 = new Point(120, 180);
+            Point p3 = new Point(125, 250);
+            Point[] curvePoints =
+            {
+                p1, p2, p3
+            };
+            drawArea.DrawPolygon(grayPen, curvePoints);
+
+            Pen greenPen = new Pen(Color.Green, 5);
+            RectangleF rect = new RectangleF(100.0F, 100.0F, 200.0F, 200.0F);
+            float startAngle = 0.0F;
+            float sweepAngle = 45.0F;
+
+            drawArea.DrawPie(greenPen, rect, startAngle, sweepAngle);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Right)
+            {
+                drawArea.Clear(Color.White);
+            }
+        }
+        public int xInit = 0, yInit = 0;
+        public int xFinal = 0, yFinal = 0;
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Right)
+            {
+                xInit = e.X;
+                yInit = e.Y;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Right)
+            {
+                xFinal = e.X;
+                yFinal = e.Y;
+
+                Pen greenPen = new Pen(Color.Green, 3);
+                Pen grayPen = new Pen(Color.Gray, 3);
+
+                Rectangle commonRect = new Rectangle(xInit, yInit, xFinal, yFinal);
+
+                drawArea.DrawRectangle(grayPen, commonRect);
+                drawArea.DrawEllipse(greenPen, commonRect);
+            }
         }
     }
 }
